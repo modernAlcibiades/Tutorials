21ed2b2809b42e5a2d05ffc157f64fb7893b5ce0
modernAlcibiades <mehul.kumar@columbia.edu>
2022-02-28 18:09:10 +0900

    Updated SMT puzzle solution based on comments

--- 02.Lesson_InvestigateViolations/ERC20/runERC20.sh
+++ 02.Lesson_InvestigateViolations/ERC20/runERC20.sh
@@ -1,6 +1,6 @@
 solc-select use 0.8.0
 
-certoraRun ERC20Bug4.sol:ERC20 --verify ERC20:ERC20.spec \
+certoraRun ERC20Bug2.sol:ERC20 --verify ERC20:ERC20.spec \
 --optimistic_loop \
 --send_only \
 --msg "$1"
