9e59885563024b7895243619dc9c1f5d25edc14e
modernAlcibiades <mehul.kumar@columbia.edu>
2022-02-28 22:24:26 +0900

    Added auction_properties_solution

--- 06.Lesson_ThinkingProperties/AuctionDemonstration/runBroken.sh
+++ 06.Lesson_ThinkingProperties/AuctionDemonstration/runBroken.sh
@@ -1,1 +1,6 @@
- certoraRun  AuctionBroken.sol:System --verify System:Auction.spec --solc solc5.12
+ solc-selct use 0.5.12
+ 
+ certoraRun  AuctionBroken.sol:System \
+    --verify System:Auction.spec \
+    --msg "06 Auction -Run Broken"
+    --send_only
--- 06.Lesson_ThinkingProperties/AuctionDemonstration/runFixed.sh
+++ 06.Lesson_ThinkingProperties/AuctionDemonstration/runFixed.sh
@@ -1,2 +1,5 @@
- certoraRun AuctionFixed.sol:System --verify System:Auction.spec --solc solc5.12
-
+  solc-selct use 0.5.12
+ 
+ certoraRun AuctionFixed.sol:System \
+    --verify System:Auction.spec \
+    --msg "06 Auction -Run Fixed
